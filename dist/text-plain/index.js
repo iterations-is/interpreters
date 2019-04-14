fd8706c8ad1d34ce81b2327593060439f3bd9a6d
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2019-04-14 21:47:06 +0200

    add: text-plain interpreter

---
+++ dist/text-plain/index.js
@@ -0,0 +1,23 @@
+(() => {
+   // Interpreter unique name
+   const name = 'text-plain';
+
+   // Render function
+   const render = (store) => {
+      // Validation
+      if (!store.title ||
+          !store.body
+      ) return 'Not valid store';
+
+      const title = `<h1>${store.title}</h1>`;
+      const body = `<p>${store.body}</p>`;
+      return `${title}${body}`;
+   }
+
+   // Declare as a loaded interpreter
+   if(!window.iterations) window.iterations = {};
+   if(!window.iterations.interpreters) window.iterations.interpreters = {};
+   window.iterations.interpreters[name] = {
+      render
+   };
+})();
